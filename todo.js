d9ce6df1a7cda42d9c69400c4ed03758799d38cb
z0l0git <zolbayar.in@gmail.com>
2023-11-30 00:13:30 +0800

    drag not complete needs fix

--- todo.js
+++ todo.js
@@ -3,7 +3,12 @@ const inprog = document.getElementById("inprog");
 const stuck = document.getElementById("stuck");
 const done = document.getElementById("done");
 
-const drag = document.getElementById("demcon");
+const targetTodo = document.getElementById("targetTodo")
+const targetProg = document.getElementById("targetProg")
+const targetStuck = document.getElementById("targetStuck")
+const targetDone = document.getElementById("targetDone")
+
+const drag = document.querySelectorAll(".dem_con");
 
 const inputBox = document.getElementById("inputBox");
 
@@ -11,40 +16,90 @@ const addbtn = document.getElementById("addbtn");
 
 const addInput = document.getElementById("input");
 
-let middle = 1;
-const element = inprog.children[middle];
 
-// function insertBefore(newEl, exEl) {
-//   const inprog = exEl.inprogElement;
-// }
+drag.forEach((el) => {
+  el.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("box", event.target.id);
+    console.log(event);
+  });
+});
 
-drag.addEventListener("dragstart", (event) => {
-  event.dataTransfer.setData("box", event.target.id);
+// target.forEach((el) => {
+//   el.addEventListener("dragover", (event) => {
+//     event.preventDefault();
+//   });
+// });
+
+// function dragGrab(){
+//   const dragId = event.dataTransfer.getData("box");
+//   const dragItem = document.getElementById(dragId);
+//   target.forEach((el) => {
+
+//       el.appendChild(dragItem);
+      
+//   });
+  
+// };
+
+inprog.addEventListener("dragover", (event) => {
+  event.preventDefault();
 });
 
 todo.addEventListener("dragover", (event) => {
   event.preventDefault();
 });
 
-todo.addEventListener("drop", (event) => {
+stuck.addEventListener("dragover", (event) => {
   event.preventDefault();
-  const dragId = event.dataTransfer.getData("box");
-  const dragItem = document.getElementById(dragId);
-  //   inprog.parentNode.insertBefore(dragItem, inprog.nextSibling);
-  todo.appendChild(dragItem);
+});
+done.addEventListener("dragover", (event) => {
+  event.preventDefault();
 });
 
-inprog.addEventListener("dragover", (event) => {
-  event.preventDefault();
+todo.addEventListener("drop", (event) => {
+  const dragId = event.dataTransfer.getData("box");
+  const dragItem = document.getElementById(dragId);
+  targetTodo.appendChild(dragItem)
+
 });
+
 
 inprog.addEventListener("drop", (event) => {
-  event.preventDefault();
   const dragId = event.dataTransfer.getData("box");
   const dragItem = document.getElementById(dragId);
+  targetProg.appendChild(dragItem)
 
-  inprog.insertBefore(dragItem, element);
-  middle++;
-  console.log(middle);
-  //   inprog.appendChild(dragItem);
 });
+
+stuck.addEventListener("drop", (event) => {
+  const dragId = event.dataTransfer.getData("box");
+  const dragItem = document.getElementById(dragId);
+  targetStuck.appendChild(dragItem)
+  
+});
+
+done.addEventListener("drop", (event) => {
+  const dragId = event.dataTransfer.getData("box");
+  const dragItem = document.getElementById(dragId);
+  targetDone.appendChild(dragItem)
+
+});
+
+
+// target.forEach((el) =>{
+//   console.log(el);
+//     el.addEventListener("dragover", (event) => {
+//     event.preventDefault();
+//   });
+
+//   el.addEventListener("drop", (event) => {
+//     event.preventDefault();
+//     const dragId = event.dataTransfer.getData("box");
+//     const dragItem = document.getElementById(dragId);
+//     inprog.parentNode.insertBefore(dragItem, inprog.nextSibling);
+//   // el.appendChild(dragItem);
+//   });
+
+// });
+
+
